3dba80e164355440f1a90b0047d52660629e921d
tushar <aabhashsingh2004@gmail.com>
2023-10-10 16:43:41 +0530

    code

---
+++ pattern6.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class pattern6 {
+    public static void main(String[] args) {
+        
+
+    System.out.println("enter number");
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    for(int i=0;i<n;i++)
+    {
+         for(int j=0;j<=n-i;j++)
+         {
+            System.out.print("  ");
+         }
+        for (int j=0;j<n;j++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    s.close();
+}
+}
+
---
+++ pattern7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class pattern7 {
+     public static void main(String[] args) {
+    System.out.println("enter number");
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    for(int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=i;j++){
+            if((j>1)&&(j!=n)&&(j<i))
+            {
+                System.out.print(" ");
+            }
+            else
+            {
+                System.out.print("* ");
+            }
+        }
+            System.out.println();
+        }
+    s.close();
+}
+}
